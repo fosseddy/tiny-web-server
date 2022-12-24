5345d39a6ee5570ad289c2dce63b4db12595bc81
fosseddy <markfosseddy@gmail.com>
2022-12-24 10:21:05 +0500

    add form for adder, change args parsing

--- cgi/adder.c
+++ cgi/adder.c
@@ -5,53 +5,34 @@
 
 int main(void)
 {
-    char *query, *p, body[256];
-    int a, b, sum;
+    char *query, *p, *arg1, *arg2, body[256];
+    int a = 0, b = 0;
 
     query = getenv("QUERY_STRING");
-    if (query == NULL) {
-        sprintf(body, "<html>"
-                      "<head><title>Adder Error</title></head>"
-                      "<body>"
-                      "<p>400 Bad Request</p>"
-                      "<p>No arguments provided to adder</p>"
-                      "</body>"
-                      "</html>\r\n");
-        printf("HTTP/1.1 400 Bad Request\r\n"
-               "Content-type: text/html\r\n"
-               "Content-length: %d\r\n\r\n", strlen(body));
-        printf("%s", body);
-        return 1;
-    }
+    if (query != NULL) {
+        p = strchr(query, '&');
+        if (p != NULL) {
+            *p++ = '\0';
 
-    p = strchr(query, '&');
-    if (p == NULL) {
-        sprintf(body, "<html>"
-                      "<head><title>Adder Error</title></head>"
-                      "<body>"
-                      "<p>400 Bad Request</p>"
-                      "<p>Adder requires 2 arguments</p>"
-                      "</body>"
-                      "</html>\r\n");
-        printf("HTTP/1.1 400 Bad Request\r\n"
-               "Content-type: text/html\r\n"
-               "Content-length: %d\r\n\r\n", strlen(body));
-        printf("%s", body);
-        return 1;
-    }
+            arg1 = strchr(query, '=');
+            if (arg1 != NULL) {
+                a = atoi(arg1 + 1);
+            }
 
-    *p++ = '\0';
-    a = atoi(query);
-    b = atoi(p);
-    sum = a + b;
+            arg2 = strchr(p, '=');
+            if (arg2 != NULL) {
+                b = atoi(arg2 + 1);
+            }
+        }
+    }
 
     sprintf(body, "<html>"
-            "<head><title>Adder</title></head>"
-            "<body><h1>%d + %d = %d</h1></body>"
-            "</html>\r\n", a, b, sum);
-    printf("HTTP/1.1 400 Bad Request\r\n"
-            "Content-type: text/html\r\n"
-            "Content-length: %d\r\n\r\n", strlen(body));
+                  "<head><title>Adder</title></head>"
+                  "<body><h1>%d + %d = %d</h1></body>"
+                  "</html>\r\n", a, b, a + b);
+    printf("HTTP/1.1 200 OK\r\n"
+           "Content-type: text/html\r\n"
+           "Content-length: %ld\r\n\r\n", strlen(body));
     printf("%s", body);
     return 0;
 }
